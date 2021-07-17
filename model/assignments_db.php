2ed5c27dd9dde8abec146a60fa8e21976cf76dba
ipakey <bornagainyfke@gmai.com>
2021-07-17 09:38:55 +0100

    css added & index commands

--- model/assignments_db.php
+++ model/assignments_db.php
@@ -3,13 +3,13 @@
 function get_assignments_by_course($course_Id){
     global $db;
     if($course_Id){
-        $query = 'SELECT A.as_Id, A.as_desc, C.cs_Name FROM assignments A LEFT JOIN courses C ON A.as_Id = C.cs_Id WHERE A.as_Id = :course_id ORDER BY A.as_Id';
+        $query = 'SELECT A.as_Id, A.as_desc, C.cs_Name FROM assignments A LEFT JOIN courses C ON A.as_courseId = C.cs_Id WHERE A.COURSEID = :course_id ORDER BY A.as_Id';
     }
     else{
-        $query = 'SELECT A.as_Id, A.as_desc, C.cs_Name FROM assignments A LEFT JOIN courses C ON A.as_Id = C.cs_Id ORDER BY C.cs_Id';
+        $query = 'SELECT A.as_Id, A.as_desc, C.cs_Name FROM assignments A LEFT JOIN courses C ON A.as_courseId = C.cs_Id ORDER BY C.cs_Id';
     }
     $statement = $db->prepare($query);
-    $statement->bindValue(':course_Id', $course_Id);
+    $statement->bindValue(':course_id', $course_Id);
     $statement->execute();
     $assignments = $statement->fetchAll();
     $statement-> closeCursor();
@@ -20,7 +20,7 @@ function delete_assignment($assignment_id){
     global $db;
     $query = 'DELETE FROM Assignments WHERE ID = :assign_id';
     $statement = $db->prepare($query);
-    $statement->bindValue(':assign_Id', $assignment_id);
+    $statement->bindValue(':assign_id', $assignment_id);
     $statement->execute();  
     $statement-> closeCursor();
 
@@ -28,10 +28,10 @@ function delete_assignment($assignment_id){
 
 function add_assignment($course_id, $description){
     global $db;
-    $query='INSERT INTO assignments (as_Desc, as_CourseId) VALUES (:descr, :course_Id)';
+    $query='INSERT INTO assignments (as_Desc, as_CourseId) VALUES (:descr, :course_id)';
     $statement = $db->prepare($query);
     $statement->bindValue(':descr', $description);
-    $statement->bindValue(':course_Id', $course_id);
+    $statement->bindValue(':course_id', $course_id);
     $statement->execute();   
     $statement-> closeCursor();
 
